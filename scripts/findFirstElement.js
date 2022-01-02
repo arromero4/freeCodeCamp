903dc001728f02234191e07e523ce26fa8b31e29
arromero4 <arromero0408@gmail.com>
2022-01-01 23:05:20 -0600

    basic algorithm scripts

---
+++ scripts/findFirstElement.js
@@ -0,0 +1,15 @@
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if(func(num)){
+      return console.log(num);
+    }
+    
+  }
+  return console.log(undefined);
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })
+findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })
