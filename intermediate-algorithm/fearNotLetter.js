a8dc7aa965480e357b70af9404dfae8cbb950c86
arromero4 <arromero0408@gmail.com>
2022-01-19 18:16:17 -0600

    actualización oficina

---
+++ intermediate-algorithm/fearNotLetter.js
@@ -0,0 +1,58 @@
+/*Missing letters
+Find the missing letter in the passed letter range 
+and return it.
+
+If all letters are present in the range, 
+return undefined. */
+
+function fearNotLetter(str) {
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    var len = str.length;
+    var start = alphabet.indexOf(str[0]);
+    
+    for(var i = start; i < start + len; i++){
+        //console.log(alphabet[i])
+      if(!str.includes(alphabet[i])){
+        
+        return alphabet[i];
+      }
+    }
+    return undefined;
+  }
+  
+fearNotLetter("abce")
+
+
+//abce
+ function fearNotLetter2(str) {
+   
+    for (var i = 0; i < str.length; i++) {
+      /* code of current character */
+      var code = str.charCodeAt(i);
+      //code = str.charCodeAt(0) = a = 97;
+      //code = str.charCodeAt(1) = b = 98;
+      //code = str.charCodeAt(2) = c = 99;
+      //code = str.charCodeAt(3) = e = 101;
+
+      console.log(str.charCodeAt(0)+i)
+
+        
+      /* if code of current character is not equal to first character + no of iteration
+          hence character has been escaped */
+
+    //str.charCodeAt(0) + 0 = str.charCodeAt(0) = a = 97
+    //str.charCodeAt(0) + 1 = str.charCodeAt(1) = b = 98
+    //str.charCodeAt(0) + 2 = str.charCodeAt(2) = c = 99
+    //str.charCodeAt(0) + 3 = str.charCodeAt(3) = d = 100
+    
+      if (code !== str.charCodeAt(0) + i) {
+
+        /* if current character has escaped one character find previous char and return */
+        return String.fromCharCode(code - 1);
+      }
+    }
+    return undefined;
+  }
+  
+  // test here
+  fearNotLetter2("abce");
