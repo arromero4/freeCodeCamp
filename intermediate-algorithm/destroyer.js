a6bf39a2adcb43c17d97241f0b9dadcba477f0de
arromero4 <arromero0408@gmail.com>
2022-01-20 12:22:14 -0600

    Oficina

--- intermediate-algorithm/destroyer.js
+++ intermediate-algorithm/destroyer.js
@@ -10,14 +10,14 @@ Note: You have to use the arguments object. */
 function destroyer(arr) {
     //gives an array from arguments and extracts from the index 1 
     let valsToRemove = Array.from(arguments).slice(1)
-    //console.log(valsToRemove)
+    console.log(valsToRemove)
     return arr.filter(val => {
         return !valsToRemove.includes(val)
     })
   }
   
   console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
-  console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
-  console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5));
-  console.log(destroyer(["tree", "hamburger", 53], "tree", 53));
-  console.log(destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan"));
+//   console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+//   console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5));
+//   console.log(destroyer(["tree", "hamburger", 53], "tree", 53));
+//   console.log(destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan"));
---
+++ intermediate-algorithm/uniteUnique.js
@@ -0,0 +1,52 @@
+/*Sorted Union
+Write a function that takes two or more arrays and 
+returns a new array of unique values in the order 
+of the original provided arrays.
+
+In other words, all values present from all arrays 
+should be included in their original order, 
+but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original 
+order, but the final array should not be sorted 
+in numerical order.
+
+Check the assertion tests for examples.
+*/
+//Option 1
+function uniteUnique(arr) {
+    let finalCollection = []
+    let values =  Object.values(arguments);
+
+    for (let i = 0; i < values.length; i++) {
+        
+        for (let j = 0; j < values[i].length; j++) {
+            if(finalCollection.indexOf(values[i][j]) === -1){
+                finalCollection.push(values[i][j])
+            }
+            
+        }
+        
+    }
+    
+    return finalCollection;
+  }
+  
+  let result = uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  //console.log(result)
+
+
+  //Option 2
+  function uniteUnique2(arr) {
+    let unionArr = [];
+    let allArr = Array.from(arguments).reduce((arr1,arr2) => arr1.concat(arr2))
+    allArr.forEach(element => {
+        if(!unionArr.includes(element)){
+            unionArr.push(element)
+        }
+    });
+    return unionArr
+  }
+  
+  let result2 = uniteUnique2([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  console.log(result2)
