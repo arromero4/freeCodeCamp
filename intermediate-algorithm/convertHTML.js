66a7860d9ed8406ea0a12f5335223e87c8244e96
arromero4 <arromero0408@gmail.com>
2022-01-21 03:14:13 -0600

    Intermediate Algorithm Scripting

--- intermediate-algorithm/convertHTML.js
+++ intermediate-algorithm/convertHTML.js
@@ -3,29 +3,71 @@ Convert the characters &, <, >, " (double quote),
 and ' (apostrophe), in a string to their 
 corresponding HTML entities. */
 
+//Option 1
 function convertHTML(str) {
-    let symbols = /[&,<,>,",']/g;
-    let found = str.match(symbols)
-    console.log(found)
-
-
-
-    for (var i = 0; i < str.length; i++) {
-        /* code of current character */
-        var code = str.charCodeAt(i);
-        //let charFromCode = String.fromCharCode(code)
-  
-
+    //let symbols = /[&,<,>,",']/g;
+    //let found = str.match(symbols)
+    const entities = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&apos;"
     }
-    
-    return str;
+
+    let arr = str.split("")
+                 .map(elem=>{
+                  if(entities.hasOwnProperty(elem)){
+                    return entities[elem]
+                  }else{
+                    return elem
+                  }
+                 })
+
+
+    return arr.join("");
 
   }
+
   
-//convertHTML("Dolce & Gabbana");
-   convertHTML("Hamburgers < Pizza < Tacos")
-//   convertHTML("Sixty > twelve")
-//   convertHTML('Stuff in "quotation marks"')
-//   convertHTML("Schindler's List")
-//   convertHTML("<>")
-//   convertHTML("abc")
+
+
+  
+  // console.log(convertHTML("Dolce & Gabbana"));
+  // console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+  // console.log(convertHTML("Sixty > twelve"));
+  // console.log(convertHTML('Stuff in "quotation marks"'));
+  // console.log(convertHTML("Schindler's List"));
+  // console.log(convertHTML("<>"));
+  // console.log(convertHTML("abc"));
+//Option 2
+  function convertHTML2(str) {
+    const entities = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&apos;"
+    }
+
+    return str.split("").map(elem=>entities[elem]||elem).join("");
+  }
+  
+  //  console.log(convertHTML2("Dolce & Gabbana"));
+  //  console.log(convertHTML2("Hamburgers < Pizza < Tacos"));
+
+   //Option 3
+   function convertHTML3(str) {
+    const entities = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&apos;"
+    }
+
+    return str.replace(/([&<>\"'])/g, match => entities[match]);
+  }
+  
+   console.log(convertHTML3("Dolce & Gabbana"));
+   console.log(convertHTML3("Hamburgers < Pizza < Tacos"));
