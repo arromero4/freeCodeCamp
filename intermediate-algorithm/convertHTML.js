129ff086a4655230c60ea64baf7eaf861ec16f1b
arromero4 <arromero0408@gmail.com>
2022-01-21 01:12:43 -0600

    oficina

--- intermediate-algorithm/convertHTML.js
+++ intermediate-algorithm/convertHTML.js
@@ -4,13 +4,28 @@ and ' (apostrophe), in a string to their
 corresponding HTML entities. */
 
 function convertHTML(str) {
+    let symbols = /[&,<,>,",']/g;
+    let found = str.match(symbols)
+    console.log(found)
+
+
+
+    for (var i = 0; i < str.length; i++) {
+        /* code of current character */
+        var code = str.charCodeAt(i);
+        //let charFromCode = String.fromCharCode(code)
+  
+
+    }
+    
     return str;
+
   }
   
-  convertHTML("Dolce & Gabbana");
-  convertHTML("Hamburgers < Pizza < Tacos")
-  convertHTML("Sixty > twelve")
-  convertHTML('Stuff in "quotation marks"')
-  convertHTML("Schindler's List")
-  convertHTML("<>")
-  convertHTML("abc")
+//convertHTML("Dolce & Gabbana");
+   convertHTML("Hamburgers < Pizza < Tacos")
+//   convertHTML("Sixty > twelve")
+//   convertHTML('Stuff in "quotation marks"')
+//   convertHTML("Schindler's List")
+//   convertHTML("<>")
+//   convertHTML("abc")
