7cc156aea1592ccbe44e4a2612c0a398f9a4c00c
arromero4 <arromero0408@gmail.com>
2022-02-05 22:01:26 -0600

    Algorithms Data Structures Projects

--- Algorithms-Data-Structures-Projects/Roman/app.js
+++ Algorithms-Data-Structures-Projects/Roman/app.js
@@ -1,10 +1,21 @@
+
+const romanInput = document.getElementById("decimalInput");
+const romanButton = document.getElementById("button");
+
+romanButton.addEventListener("click",(e)=>{
+    
+    romanInput.value = convertToRoman(e.target.value);
+    document.getElementById("demo").innerHTML = romanInput.value
+  });
+ 
+
 function convertToRoman(num) {
     var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
 
     let roman = ""
     Object.entries(lookup).forEach(([key,elem])=>{
 
-        console.log(key,elem)
+        //console.log(key,elem)
         while (num >= elem) {//16
             roman+=key//"" + X = X
                       //X + V = XV
@@ -16,6 +27,7 @@ function convertToRoman(num) {
     })
     
     return roman;//XVI
+    
    }
    
-console.log(convertToRoman(16))
+   
