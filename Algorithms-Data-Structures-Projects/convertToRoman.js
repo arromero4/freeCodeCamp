ebb552c5769047b24efdcf52f01b0047ddb42594
arromero4 <arromero0408@gmail.com>
2022-02-12 01:22:55 -0600

    Algorithms Data Structures Projects

--- Algorithms-Data-Structures-Projects/convertToRoman.js
+++ Algorithms-Data-Structures-Projects/convertToRoman.js
@@ -1,3 +1,10 @@
+/*Roman Numeral Converter
+Convert the given number into a 
+roman numeral.
+
+All roman numerals answers should be provided 
+in upper-case.*/
+
 function convertToRoman(num) {
     var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
 
