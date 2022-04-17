1072b0592e08ea0aa0a05092aecd9ed94420a17a
arromero4 <arromero0408@gmail.com>
2022-04-17 00:48:49 -0500

    Learning React

--- Algorithms-Data-Structures-Projects/convertToRoman.js
+++ Algorithms-Data-Structures-Projects/convertToRoman.js
@@ -1,12 +1,37 @@
 function convertToRoman(num) {
-    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+    //16
+    var lookup = {
+        M:1000,
+        CM:900,
+        D:500,
+        CD:400,
+        C:100,
+        XC:90,
+        L:50,
+        XL:40,
+        X:10,
+        IX:9,
+        V:5,
+        IV:4,
+        I:1}
 
- 
+ let roman = ""
     Object.entries(lookup).forEach(([key,elem])=>{
-        console.log(key,elem)
+        //console.log(elem)
+        while(num>=elem){
+            
+            roman+=key //"" + X = X
+                        //X + V = XV 
+                        //XV + I = XVI
+            num-=elem //16-10 = 6
+                      //6-5= 1
+                      //1-1 = 0
+  
+            console.log(num)
+        }
+        
     })
     
-    return num;
+    return roman;// Returns XVI
    }
-   
-convertToRoman(2)
+convertToRoman(16)
